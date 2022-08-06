384752856984c018af08c3d46a6723e059a7d267
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-08-06 16:47:19 +0300

    Add files via upload

---
+++ heconsole/__init__.py
@@ -0,0 +1,12 @@
+from heconsole.console import Console
+from colorama import Fore as textColor
+from colorama import Back as bgColor
+console = Console()
+
+__version__ = '1.0'
+__description__ = 'perfect pip module to design and display your terminal, great customization'
+__author__ = 'Misha Korzhik'
+__author_email__ = 'developer.mishakorzhik@gmail.com'
+__homepage__ = 'https://github.com/mishakorzik/heconsole'
+__license__ = 'MIT'
+name = "heconsole"
